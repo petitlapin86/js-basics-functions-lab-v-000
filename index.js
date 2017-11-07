4bdb9b333e06b2eaf0d9d451bc8ec73bbb8ed766
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 16:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+describe('index.js', function() {
+  describe('distanceFromHqInBlocks()', function() {
+    it('returns a distance in blocks', function() {
+      expect(distanceFromHqInBlocks(43)).to.equal(1);
+    });
+
+    it('returns a distance in blocks', function() {
+      expect(distanceFromHqInBlocks(50)).to.equal(8);
+    });
+
+    it('calculates distances below 42nd street', function() {
+      expect(distanceFromHqInBlocks(34)).to.equal(8);
+    });
+  });
+
+
+  function distanceFromHqInBlocks(blocks) {
+    if (blocks <= 42) {
+      return blocks;
+  }
