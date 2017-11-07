fe3d010eee677e219f57f06d02c8aaca33c67c54
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 17:37:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,4 +24,7 @@ function distanceTravelledInFeet (start, destination) {
 
 function calculatesFarePrice (start, destination) {
     const distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+      return 0;
+    } 
 }
