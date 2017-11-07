61ec9c0a3dd55f7353de3480b8e689f47efef961
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-07 17:12:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
   function distanceFromHqInBlocks (blocks) {
     if (blocks > 42) {
       return blocks - 42; }
-  else (blocks < 42) {
-    return 42 - blocks;
+  else return 42 - blocks;
   }
 }
